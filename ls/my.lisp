f209a54ae64812c66f00f32a9ca18b909a8b84c7
it team <itsupport@telecon-systems.com>
2019-05-28 17:37:25 +0530

    lisp cart completed

--- ls/my.lisp
+++ ls/my.lisp
@@ -6,9 +6,117 @@
 
 (defvar pasta 0)
 
+
+(defvar name)
+
+(defvar email)
+
+(defvar address)
+
+(setq alist-of-colors
+           '((rose . red) (lily . white) (buttercup . yellow)))
 ;(cond ((> a 20)
 ;   (format t "~% a is greater than 20"))
 ;   (t (format t "~% value of a is ~d " a)))
+
+(defun getPaymentInfo ()
+    (format T "~%~a~%" alist-of-colors)
+)
+
+
+
+(defun getPzPrice(qty)
+    (if (= qty 1)
+      (return-from getPzPrice 12)
+      12
+    )
+    (if (= qty 2)
+(defvar *valid-sports* (list "Athletics" "Baseball" "Cricket") )      (return-from getPzPrice 22)
+      22
+    )
+    (if (> qty 2)
+      (return-from getPzPrice (* qty 10))
+    )
+)
+
+
+(defun getPsPrice(qty)
+    (if (= qty 1)
+      (return-from getPsPrice 8)
+      12
+    )
+    (if (= qty 2)
+      (return-from getPsPrice 15)
+      22
+    )
+    (if (> qty 2)
+      (return-from getPsPrice (* qty 7))
+    )
+)
+
+(defun getGB (qty)
+    (return-from getGB (floor qty 3))
+)
+
+(defun getBK (gb drink)
+    (setq gb (floor gb 3)) 
+    (setq drink (floor drink 3))
+    (if (> gb drink)
+        drink
+        gb
+    )
+)
+
+
+(defun getCart ()
+    (defvar gtotal 0)
+    (defvar gb 0)
+    (defvar drink 0)
+    (defvar choice)
+    (progn
+        (format T "~%------------------------------Cart-----------------------------~%")
+        (format T "~%Item            Quantity    Total")
+        (if (> pizza 0)
+            (progn
+                (format T "~%Pizza           ~a           ~a~%" pizza (getPzPrice pizza))
+                (setq gtotal (+ gtotal (getPzPrice pizza)))
+            )
+        )
+        (if (> pasta 0)
+            (progn
+                (format T "~%Pasta           ~a          ~a~%" pasta (getPsPrice pasta))
+                (setq gtotal (+ gtotal (getPsPrice pasta)))
+            )
+        )
+        (if (> pizza 2)
+            (format T "~%Garlic Bread    ~a           0~%" (getGB pizza))
+        )
+        (if (> pasta 2)
+            (format T "~%Soft Drink      ~a           0~%" (getGB pasta))
+        )
+        (if (and (> pizza 8)
+                 (> pasta 8))
+            (format T "~%Baklava         ~a          0" (getBK (floor pizza 3) (floor pasta 3)))
+        )
+        (format T "~%                       ----------------------")
+        (format T "~%Grand Total is               ~a~%" gtotal)
+        (format T "~%~%1. Checkout~%2. Menu")
+        (format T "~%Enter Choice : ")
+        (setq choice (read))
+        (if (= choice 1)
+            (progn
+                (princ "Enter Name: ")
+                (setq name (read))
+                (princ "Enter Email: ")
+                (setq email (read))
+                (princ "Enter Address: ")
+                (setq address  (read))
+                (princ "~%You order confirmed~%")
+            )
+        )
+    ) 
+)
+
 
 (defun Select-Option (option)
 	(if	(= option 1)
@@ -24,6 +132,25 @@
 			(print "Please Enter Quantity : ")
 			(setq pizza (read)))
 	)
+    (if (= option 4)
+        (setq pasta 1)
+        ;(format T "pizza is : ~a~%" pizza)
+    )
+    (if (= option 5)
+        (setq pasta 5)
+        ;(format T "pizza is : ~a~%" pizza)
+    )
+    (if (= option 6)
+        (progn
+            (print "Please Enter Quantity : ")
+            (setq pasta (read)))
+    )
+    (if (= option 7)
+        (getCart)
+    )
+    (if (= option 8)
+        (getPaymentInfo)
+    )
 )
 
 (defun Menu ()
@@ -40,7 +167,6 @@
 			(format T "Invalid choice - please choose again.~%") ; THEN part
 			(Select-Option option)                            ; ELSE part
 		)
-		(format T "pizza is : ~a~%" pizza)
 	)
 )   
    
